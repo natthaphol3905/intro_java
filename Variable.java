b23443e86670f4dd9dbb417a87757766f8203608
Your Name <slaranatt093082@gamil.com>
2019-08-03 14:19:30 +0700

    Variable Java

---
+++ Variable.java
@@ -0,0 +1,18 @@
+public class Variable {
+  public static void main(String[] args) {
+       int number = 10;
+       long longNumber = 100;
+        double doubleNumber = 1.5;
+        float floatNumder = 3.0f;
+
+        char charText = 'C';
+        String text ="message";
+
+        System.out.println(number);
+        System.out.println(longNumber);
+        System.out.println(doubleNumber);
+        System.out.println(floatNumder);
+        System.out.println(charText);
+        System.out.println(text);
+    }
+}
